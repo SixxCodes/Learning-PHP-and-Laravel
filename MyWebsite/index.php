0bb7e0b21215db9885a482a48ad58e8d8d49f58b
SixxCodes <z.madayag.141776.tc@umindanao.edu.ph>
2025-02-18 17:54:10 -0800

    Initial Commit.

---
+++ MyWebsite/index.php
@@ -0,0 +1,19 @@
+<!DOCTYPE html>
+<html lang="en">
+    <head>
+        <meta charset="UTF-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1.0">
+        <title>Document</title>
+    </head>
+    <body>
+        <?php
+            //open
+            if (true) { 
+        ?>
+                <p>Hello, World!</p>
+        <?php 
+            }
+            //close
+        ?>
+    </body>
+</html>
